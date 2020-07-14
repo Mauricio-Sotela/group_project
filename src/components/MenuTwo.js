7e79b8064a532a8720e77d7769a4700e660e3094
Kevin <kev.saintigny.schmidt@gmail.com>
2020-07-14 22:23:07 +0200

    fixed button & routing & resolve bugs

--- src/components/MenuTwo.js
+++ src/components/MenuTwo.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
-// import CartContainer from './CartContainer'
 
 const MenuTwo = (state) => {
   return (
