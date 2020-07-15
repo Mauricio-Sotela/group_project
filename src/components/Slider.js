64f2397d23e7462eb5413a5728de74e6c8e2023b
sotela88 <j-ms-27@hotmail.com>
2020-07-15 17:20:17 +0200

    carousel

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -28,22 +28,22 @@ export default class PauseOnHover extends Component {
           </div>
         <Slider {...settings}>
           <div>
-          <img src='https://images.unsplash.com/photo-1513530534585-c7b1394c6d51?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80' width='30%'/>
+          <img className='ban' src='https://images.unsplash.com/photo-1513530534585-c7b1394c6d51?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80' width='30%'/>
           </div>
           <div>
-            <h3>2</h3>
+          <img className='ban' src='https://images.unsplash.com/photo-1511649475669-e288648b2339?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80' width='30%'/>
           </div>
           <div>
-            <h3>3</h3>
+           <img className='ban' src='https://images.unsplash.com/photo-1519219788971-8d9797e0928e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1328&q=80' width='30%'/>
           </div>
           <div>
-            <h3>4</h3>
+            <img className='ban' src='https://images.unsplash.com/photo-1528297506728-9533d2ac3fa4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80' width='30%'/>
           </div>
           <div>
-            <h3>5</h3>
+            <img className='ban' src='https://images.unsplash.com/photo-1493934558415-9d19f0b2b4d2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1336&q=80' width='30%'/>
           </div>
           <div>
-            <h3>6</h3>
+            <img className='ban' src='https://images.unsplash.com/photo-1505330622279-bf7d7fc918f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80' width='30%'/>
           </div>
         </Slider>
       </div>
